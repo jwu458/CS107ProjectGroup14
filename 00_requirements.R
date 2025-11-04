dccdbee9b8d110a824f64e1b9857c26a8fdcaa91
jwu458 <jwu458@ucr.edu>
2025-11-04 15:45:01 -0800

    Added libraries to the requirements file

---
+++ 00_requirements.R
@@ -0,0 +1,8 @@
+if (require(tidyverse)) {       ## If package is not installed  
+  install.packages("tidyverse") ## Install
+  library(tidyverse)            ## Load package to environment.
+}
+if (require(magrittr)) {       ## If package is not installed  
+  install.packages("magrittr") ## Install
+  library(magrittr)            ## Load package to environment.
+}
