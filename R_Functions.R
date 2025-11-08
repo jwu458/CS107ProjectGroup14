0d19def809443614fec2b6e24bc63eb3290170da
swoo034 <swoo034@ucr.edu>
2025-11-07 22:17:33 -0800

    .

--- R_Functions.R
+++ R_Functions.R
@@ -1,14 +1,12 @@
 
 
-plot(x = cleanData$Age , y = cleanData$HOMA , xlab = "age", ylab = "HOMA")
+plot(x = cleanData$Age , y = cleanData$HOMA , xlab = "age", ylab = "HOMA", main = "Graph of Age and HOMA", sub = "how age is related to HOMA")
 
 abline(h = 2.5, col = "red")
 
 
 
-
-plot(x = cleanData$BMI , y = cleanData$HOMA, xlab = "BMI", ylab = "HOMA")
+plot(x = cleanData$BMI , y = cleanData$HOMA, xlab = "BMI", ylab = "HOMA", main = "Graph of HOMA and BMI", sub = "How BMI is affecting HOMA")
 
 abline(h = 2.5, col = "red")
-
 
